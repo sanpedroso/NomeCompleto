e9f5dd0e167b3bbf0843812d10233798f9529867
= <=>
2022-09-13 23:33:18 -0300

    add exercicio nome completo

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string nome, sobrenome;
+Console.Write("digite seu nome");
+nome = Console.ReadLine()!;
+
+Console.WriteLine("digite seu nome");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
